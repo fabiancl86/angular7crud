07d5334f528fe777011c2aef0bbf925c5e929f11
Fabian Camacho <fabiancl86@gmail.com>
2019-09-01 16:57:21 -0400

    Subo la estructura del proyecto al repositorio de GitHub

---
+++ api/DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb://localhost:27089/ng7crud'  //nombre de la BD
+ };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms'; //para validar forms
+import { HttpClientModule } from '@angular/common/http';
+import { BusinessService } from './business.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GstAddComponent } from './gst-add/gst-add.component'; //auto generado con terminal ng g
+import { GstGetComponent } from './gst-get/gst-get.component'; //auto generado con terminal ng g
+import { GstEditComponent } from './gst-edit/gst-edit.component'; //auto generado con terminal ng g
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GstAddComponent, //auto generado con terminal ng g
+    GstGetComponent, //auto generado con terminal ng g
+    GstEditComponent //auto generado con terminal ng g
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule, // libreria para loading
+    ReactiveFormsModule, //valida forms
+    HttpClientModule,
+    
   ],
-  providers: [],
+  providers: [BusinessService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
